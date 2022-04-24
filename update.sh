d9384160c4200535e22050ba643003ff4e615d1c
owl <97215481+coolestowl@users.noreply.github.com>
2022-04-24 10:33:50 +0800

    fix performance

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 cd /Applications/V2rayU.app/Contents/Resources/
 
-gh repo view XTLS/Xray-core --json latestRelease --jq .latestRelease.tagName
+tag=`gh repo view XTLS/Xray-core --json latestRelease --jq .latestRelease.tagName`
 
-wget -O Xray-macos-64.zip https://github.com/XTLS/Xray-core/releases/download/`gh repo view XTLS/Xray-core --json latestRelease --jq .latestRelease.tagName`/Xray-macos-arm64-v8a.zip
+echo "latest tag ${tag}"
+
+wget -O Xray-macos-64.zip https://github.com/XTLS/Xray-core/releases/download/${tag}/Xray-macos-arm64-v8a.zip
 
 ./unzip.sh
 
