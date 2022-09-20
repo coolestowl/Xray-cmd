9f4f55bc6b90eb0672e6f408075d914dec01d566
owl <coolestowl@users.noreply.github.com>
2022-09-20 20:49:28 +0800

    add ignite.sh and stop-v2rayu.sh

---
+++ ignite.sh
@@ -0,0 +1,1 @@
+cd ~/.V2rayU && ./v2ray-core/xray -config config.json
---
+++ stop-v2rayu.sh
@@ -0,0 +1,4 @@
+# https://github.com/yanue/V2rayU/issues/1097#issuecomment-1229214909
+
+/bin/launchctl remove yanue.v2rayu.v2ray-core
+/bin/launchctl remove yanue.v2rayu.http
