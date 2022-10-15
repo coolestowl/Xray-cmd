2bff32ff34ba38e28f61c5d77a04866fc119cbbd
owl <coolestowl@users.noreply.github.com>
2022-10-15 23:56:44 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
+# on server
+
+# TODO: sed -i // /etc/ssh/sshd_config # deny password login
+# systemctl restart sshd.service
+
+apt update && apt install docker.io
+
+reboot
+
+# upload config files TODO: generate config file with random parameters
+
+cd /root && tar -xzvf hhh.tar.gz
+
 docker run -d -p 2333:2333 --name xray --restart=unless-stopped -v /root/conf:/etc/xray -v /root/certs:/certs teddysun/xray
